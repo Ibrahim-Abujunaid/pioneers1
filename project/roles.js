89e775d6c2af796908350ba61247685781e68109
developer <developer@Pioneerstech.local>
2024-06-12 16:57:06 +0300

    hi bro

--- project/roles.js
+++ project/roles.js
@@ -1,78 +1,66 @@
-// Declare the Swiper instance variable outside the function scope
-var swiper;
-
-// Function to initialize Swiper
-function initSwiper() {
-  swiper = new Swiper('.swiper-container', {
-    breakpoints: {
-      // when window width is <= 1806px
-      1400: {
-        slidesPerView: 4,
-        spaceBetween: 20
-      },
-      992: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 20
-      },
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 50
-      },
-     
-    pagination: {
-      el: '.swiper-pagination',
-      reverseDirections: true,
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-      // You can add more breakpoints here if needed
-    }
-  });
-}
-
-const slide_elements = document.querySelectorAll('.first-property');
-
-slide_elements.forEach(element => {
-
+document.addEventListener("DOMContentLoaded", function() {
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 'auto',
+  spaceBetween: 10,
+  // breakpoints: {
+  //   1400: {
+  //     slidesPerView: 4,
+  //     spaceBetween: 20
+  //   },
+  //   992: {
+  //     slidesPerView: 3,
+  //     spaceBetween: 20
+  //   },
+  //   768: {
+  //     slidesPerView: 2,
+  //     spaceBetween: 20
+  //   },
+  //   320: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 50
+  //   }
+  // }
   
-  element.addEventListener('click', (e) => {
-    console.log(e.currentTarget);
-  });
 });
 
-function expandDiv(element) {
+  // Get all swiper slides
+  var swiperSlides = document.querySelectorAll('.swiper-slide');
 
-// console.log(element.closest('.swiper-slide'));  
+  // Set initial flex property to each slide
+  swiperSlides.forEach(function(slide) {
+    slide.style.flex = '0 0 25%'; // Set all slides to take 1/4th of the container
+  });
 
-element.closest('.swiper-slide').style.width = "100%";  
+  // Get all divs with the 'first-property' class
+  var propertyDivs = document.querySelectorAll('.first-property');
 
-element.classList.toggle('expanded');
+  // Add click event listener to each div
+  propertyDivs.forEach(function(div, index) {
+    div.addEventListener('click', function() {
+      // Remove the 'expanded' class from all other divs
+      propertyDivs.forEach(function(otherDiv) {
+        if (otherDiv !== div) {
+          otherDiv.classList.remove('expanded');
+          otherDiv.parentElement.style.flex = '0 0 25%'; // Set other slides to take 1/4th of the container
+        }
+      });
 
-return false;
+      // Toggle the 'expanded' class on the clicked div
+      div.classList.toggle('expanded');
 
-  
+      if (div.classList.contains('expanded')) {
+        div.parentElement.style.flex = '0 0 100%'; // Set the clicked slide to take the full width (4 slides)
+        document.querySelector('.down').style.height = '900px';
+      } else {
+        div.parentElement.style.flex = '0 0 25%'; // Set the clicked slide back to take 1/4th of the container
+        document.querySelector('.down').style.height = '';
+      }
 
+      // Update Swiper to apply the changes
+      swiper.update();
 
-  // if (element.classList.contains('expanded')) {
-  //   // If expanded, show only 1 slid e
-  //   swiper.params.slidesPerView = 1;
-  // } else {
-  //     swiper.params.slidesPerView = 4;
-    
-  // }
-
-  // Update Swiper to reflect the changes
-  swiper.update();
-}
-
-// Initialize Swiper when the document is ready
-document.addEventListener("DOMContentLoaded", function() {
-  initSwiper();
+      // Slide to the clicked slide
+      swiper.slideTo(index);
+    });
+  });
 });
